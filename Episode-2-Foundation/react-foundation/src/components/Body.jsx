81cd40b6a8e7a255ef039e9c8cba2227721e508a
tausifcoder1111 <tausifcoder@gmail.com>
2026-02-04 18:59:23 +0530

    react hooks

--- Episode-2-Foundation/react-foundation/src/components/Body.jsx
+++ Episode-2-Foundation/react-foundation/src/components/Body.jsx
@@ -1,22 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 import restaurants from "./restaurants";
 
 const Body = () => {
+  const [restaurantsData, setRestaurantsData] = useState(restaurants);
 
-    const [restaurantsData , setRestaurantsData] = useState(restaurants)
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.9114575&lng=75.3186241&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+    );
+    const json = await data.json();
+    console.log("JSON", json);
+  };
 
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const filterHandler = ()=>{
-       const filterdList = restaurants.filter(
-        (res) => res.rating >= 4.5
-       )
-       setRestaurantsData(filterdList)
-    }
+  const filterHandler = () => {
+    const filterdList = restaurants.filter((res) => res.rating >= 4.5);
+    setRestaurantsData(filterdList);
+  };
 
-    const showAll = () =>{
-        setRestaurantsData(restaurants)
-    }
+  const showAll = () => {
+    setRestaurantsData(restaurants);
+  };
 
   return (
     <>
@@ -25,10 +33,10 @@ const Body = () => {
 
         {/* Filter */}
         <button className="filter-btn" onClick={filterHandler}>
-            Top Rated Restaurants
+          Top Rated Restaurants
         </button>
-          <button className="filter-btn" onClick={showAll}>
-            Show All
+        <button className="filter-btn" onClick={showAll}>
+          Show All
         </button>
 
         {/* Restaurant Container */}
