41b8e8b101cb52af1add2316ff245bd42c00ee36
tausifcoder1111 <tausifcoder@gmail.com>
2026-02-16 19:23:20 +0530

    async js

---
+++ AsyncJS/script.js
@@ -0,0 +1,31 @@
+// console.log("Namaste Javascript")
+
+// setTimeout(function(){
+//     console.log("Printing After 5 Sec.")
+// },5000)
+
+// console.log("Callback ")
+
+
+// const cart = ["LED","Laptop","Mobile","TV"]
+
+
+// function createOrder(proceedToPayment){
+
+//     console.log("Order Is Created!")
+//     // Once we create the order then only we need to proceed to payment
+//     proceedToPayment();
+// }
+
+// createOrder(proceedToPayment);
+
+// function proceedToPayment(){
+//     console.log("Proceed to payment")
+
+// }
+
+
+
+const GITHUB_URL = "https://github.com/akshaymarch7"
+const user = fetch(GITHUB_URL)
+console.log(user)
