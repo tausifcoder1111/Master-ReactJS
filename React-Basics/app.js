10c52b4d1649821ce1eb630df9df240cbf969425
tausifcoder1111 <tausifcoder@gmail.com>
2026-03-18 23:18:31 +0530

    React

---
+++ React-Basics/app.js
@@ -0,0 +1,11 @@
+// const heading = document.createElement("h1");
+// heading.innerHTML = "Hello World From Javascript";
+// const root = document.getElementById("root");
+// root.appendChild(heading)
+
+
+
+
+const heading = React.createElement("h1", {}, "Hello world from React");
+
+const root = ReactDOM.createRoot()
